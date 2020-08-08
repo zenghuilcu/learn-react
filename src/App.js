0bb3a4ecae84b8d8d901e36e1058ff351a3aeabb
sunmingdong <577769190@qq.com>
2020-08-08 11:44:53 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css'
-import {Layout, Menu, Breadcrumb} from "antd";
-
-const {Header, Content, Footer} = Layout;
 
 function App() {
-
     return (
         <div>
-            HelloWorld!
+            Hello World!
         </div>
     );
 }
