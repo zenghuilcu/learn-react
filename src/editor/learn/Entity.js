9086b8bca93aedc4fcded2162caeb3e69e968fd0
sun <577769190@qq.com>
2020-08-21 00:12:28 +0800

    update

--- src/editor/learn/Entity.js
+++ src/editor/learn/Entity.js
@@ -1,1 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import {Editor, Modifier} from "primereact/editor";
+
+function MyEditor(props) {
+    const [editorState, setEditorState] = React.useState(() => editorState.createEmpty())
+
+    const contentState = editorState.getCurrentContent();
+    const contentStateWithEntity = contentState.createEntity("LINK", "MUTABLE",
+        {url: "http://www.baidu.com"})
+    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+    const contentStateWithLink = Modifier.applyEntity(
+        contentStateWithEntity,
+        selectionState,
+        entityKey,
+    );
+    return <Editor editorState={editorState} onChange={setEditorState}/>
+}
