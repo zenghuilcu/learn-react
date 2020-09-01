e796a22203401b0dc8e0c5fb04edffc4a815f84f
sunmingdong <577769190@qq.com>
2020-09-01 21:05:41 +0800

    :bulb: update

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { Component } from 'react';
 
-export const AppFooter = () => {
-    return (
-        <div>
-            <span>PrimeReact</span>
-            <span>Theme and Layout</span>
-        </div>
-    )
+export class AppFooter extends Component {
+    render() {
+        return  (
+            <div className="layout-footer">
+                <span className="footer-text" style={{'marginRight': '5px'}}>PrimeReact</span>
+                <img src="assets/layout/images/logo.svg" alt="" width="80"/>
+                <span className="footer-text" style={{'marginLeft': '5px'}}>Theme and Layout</span>
+            </div>
+        );
+    }
 }
--- src/AppMenu.js
+++ src/AppMenu.js
@@ -1,26 +1,29 @@
-import React, {Component, Fragment} from "react";
+import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom'
-import App from "./App";
+import classNames from 'classnames';
 
 class AppSubmenu extends Component {
     constructor(props) {
         super(props);
-        this.state = {activeIndex: null}
+        this.state = {activeIndex: null};
     }
 
     onMenuItemClick(event, item, index) {
+        //avoid processing disabled items
         if (item.disabled) {
-            event.preventDefault()
+            event.preventDefault();
             return true;
         }
+
+        //execute command
         if (item.command) {
-            item.command({originalEvent: event, item: item})
+            item.command({originalEvent: event, item: item});
         }
 
         if (index === this.state.activeIndex)
-            this.setState({activeIndex: null})
+            this.setState({activeIndex: null});
         else
-            this.setState({activeIndex: index})
+            this.setState({activeIndex: index});
 
         if (this.props.onMenuItemClick) {
             this.props.onMenuItemClick({
@@ -30,10 +33,9 @@ class AppSubmenu extends Component {
         }
     }
 
-
     renderLinkContent(item) {
-        let submenuIcon = item.items && <i>Icon</i>
-        let badge = item.badge && <span>{item.badge}</span>
+        let submenuIcon = item.items && <i className="pi pi-fw pi-angle-down menuitem-toggle-icon"/>;
+        let badge = item.badge && <span className="menuitem-badge">{item.badge}</span>;
 
         return (
             <React.Fragment>
@@ -42,7 +44,7 @@ class AppSubmenu extends Component {
                 {submenuIcon}
                 {badge}
             </React.Fragment>
-        )
+        );
     }
 
     renderLink(item, i) {
@@ -50,42 +52,45 @@ class AppSubmenu extends Component {
 
         if (item.to) {
             return (
-                <NavLink to={item.to}
-                         onClick={(e) => this.onMenuItemClick(e, item, i)} exact target={item.target}>
+                <NavLink activeClassName="active-route" to={item.to} onClick={(e) => this.onMenuItemClick(e, item, i)}
+                         exact target={item.target}>
                     {content}
                 </NavLink>
             )
         } else {
             return (
-                <a href={item.url}
-                   onClick={(e) => this.onMenuItemClick(e, item, i)}
-                   target={item.target}>
+                <a href={item.url} onClick={(e) => this.onMenuItemClick(e, item, i)} target={item.target}>
                     {content}
                 </a>
-            )
+            );
+
         }
     }
 
     render() {
         let items = this.props.items && this.props.items.map((item, i) => {
             let active = this.state.activeIndex === i;
+            let styleClass = classNames(item.badgeStyleClass, {'active-menuitem': active && !item.to});
+
             return (
-                <li key={i}>
-                    {item.items && this.props.root === true && <div className={"arrow"}/>}
+                <li className={styleClass} key={i}>
+                    {item.items && this.props.root === true && <div className='arrow'/>}
                     {this.renderLink(item, i)}
                     <AppSubmenu items={item.items} onMenuItemClick={this.props.onMenuItemClick}/>
                 </li>
-            )
-        })
-        return items ? <ul className={this.props.className}>{items}</ul> : null
+            );
+        });
+
+        return items ? <ul className={this.props.className}>{items}</ul> : null;
     }
 }
 
 export class AppMenu extends Component {
     render() {
         return (
-            <div>
-                <AppSubmenu items={this.props.model} onMenuItemClick={this.props.onMenuItemClick} root={true}/>
+            <div className="layout-menu-container">
+                <AppSubmenu items={this.props.model} className="layout-menu"
+                            onMenuItemClick={this.props.onMenuItemClick} root={true}/>
             </div>
         );
     }
