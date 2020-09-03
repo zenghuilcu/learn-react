91434bbaf0b38768047459ec9d74c085d1839d4f
sun <577769190@qq.com>
2020-09-03 23:52:00 +0800

    update

--- src/my-site/WorkPage.js
+++ src/my-site/WorkPage.js
@@ -2,12 +2,23 @@ import React, {useState} from "react";
 import {InputTextarea} from "primereact/inputtextarea";
 import {Button} from "primereact/button";
 import {Divider} from "@blueprintjs/core";
+import axios from "axios"
 
 export const WorkPage = () => {
     const [value, setValue] = useState("")
 
     const submit = () => {
-
+        const uri = "http://localhost:8282/item/insertOne";
+        axios.post(uri, {
+            content: value,
+            createTime: Date.now()
+        })
+            .then(function (response) {
+                console.log(response.status)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
     }
 
     return (
@@ -15,11 +26,23 @@ export const WorkPage = () => {
             <div className="p-col">
                 <div className="p-grid">
                     <div className="p-col-6 day-box">
-
+                        <div className="p-grid p-dir-col">
+                            <div className="p-col">
+                                Today
+                            </div>
+                        </div>
                     </div>
                     <Divider/>
                     <div className="p-col">
-
+                        <div className="p-grid p-dir-col">
+                            <div className="p-col">
+                                Yesterday
+                            </div>
+                            <Divider/>
+                            <div className="p-col">
+                                Tomorrow
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
