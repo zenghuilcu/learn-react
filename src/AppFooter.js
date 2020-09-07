1c24b6af64c4498f167217277c93b88d7f846072
sunmingdong <577769190@qq.com>
2020-09-07 21:20:31 +0800

    update

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export class AppFooter extends Component {
     render() {
-        return  (
+        return (
             <div className="layout-footer">
                 <span className="footer-text" style={{'marginRight': '5px'}}>PrimeReact</span>
                 <img src="assets/layout/images/logo.svg" alt="" width="80"/>
--- src/my-site/WebSocketPage.js
+++ src/my-site/WebSocketPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Button} from "primereact/button";
 import SockJS from "sockjs-client"
 import Stomp from "stompjs"
@@ -8,18 +8,18 @@ const WEB_SOCKET_URL = " http://localhost:1111/chat"
 
 export const WebSocketPage = () => {
     const [log, setLog] = useState([])
-    let stompClient
+    const [stompClient, setStompClient] = useState(null)
+
 
     const connect = () => {
-        let socket = new SockJS(WEB_SOCKET_URL)
-        stompClient = Stomp.over(socket)
+        const socket = new SockJS(WEB_SOCKET_URL)
+        const stompClient = Stomp.over(socket)
         stompClient.connect({}, function (frame) {
-            setLog([...log, "status: " + frame])
-            stompClient.subscribe("/topic/greetings", function (greeting) {
-                console.log("Greeting")
-                showGreeting(JSON.parse(greeting.body).content)
+            setStompClient(stompClient)
+            addLog(frame)
+            stompClient.subscribe("/topic/greeting", function (greeting) {
+                addLog(JSON.parse(greeting.body).content)
             })
-            sendMessage()
         })
     }
 
@@ -33,25 +33,33 @@ export const WebSocketPage = () => {
     const sendMessage = () => {
         if (stompClient !== null) {
             stompClient.send("/app/hello", {}, JSON.stringify({"name": "Hello World!"}))
+            addLog("send message: /app/hello", JSON.stringify({"name": "Hello World"}))
         }
     }
 
-    const showGreeting = (message) => {
-        setLog([...log, message])
+    const addLog = (message) => {
+        setLog(prevState => {
+            let current = prevState;
+            console.log(current)
+            current.push(message.toString())
+            return current
+        })
     }
+
 
     return (
         <div className="p-grid">
             <div className="p-col-5">
                 <Button value="connection" onClick={connect} label="Connect"/>
                 <Button label="Send Message" onClick={sendMessage}/>
+                <Button label="Add Log" onClick={() => addLog(1)}/>
             </div>
             <div className="p-col-5">
                 <div>
                     <Button label={"Clear"} style={{float: "right"}} onClick={() => {
                         setLog([])
                     }}/>
-                    Event Log <span className="menuitem-badge">2</span>
+                    Event Log <span className="menuitem-badge">{log.length}</span>
                 </div>
                 <div>
                     <pre>
